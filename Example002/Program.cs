13437d6b6f9a839108a878188791539331da776c
Ludmila <ludmila0704@yandex.ru>
2022-07-08 16:32:37 +0300

    Ex002 name

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("еще раз Hello, World!");
+Console.WriteLine("Введите ваше имя") ;
+string username = Console.ReadLine() ;
+Console.Write("Привет, ") ; 
+Console.Write(username) ;
