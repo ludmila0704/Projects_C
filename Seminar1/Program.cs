6fb0ba7c3e39731e0103bcb18c10291090b5952f
Ludmila <ludmila0704@yandex.ru>
2022-07-08 21:30:27 +0300

    Seminar1 task1

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,4 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число");
-int a = Console.ReadLine();
-Console.WriteLine(a*a);
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+Console.Clear();
+Console.Write("Введите целое число");
+int num = Convert.ToInt32(Console.ReadLine());
+//int a =4;
+int res = num*num;
+
+//string a = Console.ReadLine();
+//Console.WriteLine("");
+Console.WriteLine($"квадрат числа {num}={res}");
