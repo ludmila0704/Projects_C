9e13a37c713cc0d495b621d3ed213d0640519499
Ludmila <ludmila0704@yandex.ru>
2022-07-08 17:19:46 +0300

    Ex003 masha

---
+++ Example003/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.Write("ура Маша");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.Write(username);
+}
