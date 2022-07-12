a314bc5d7d1e05ac6603bd5600121efa8b899170
Ludmila <ludmila0704@yandex.ru>
2022-07-12 14:33:32 +0300

    Example 010 find

---
+++ Example010_find/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {24,54,154,8,5,68,69,4,10};
+
+int n = array.Length;
+int find =4;
+
+int index=0;
+while (index<n)
+{
+    if(array[index]==find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
