590548da9b6cabe9cd5eabf729f8a3d77f49a982
Ludmila <ludmila0704@yandex.ru>
2022-07-12 14:23:56 +0300

    Example 009_array

---
+++ Example009_array/Program.cs
@@ -0,0 +1,19 @@
+﻿int Max(int arg1,int arg2,int arg3)
+{
+    int result=arg1;
+    if (arg2>result) result=arg2;
+    if (arg3>result) result =arg3;
+    return result; 
+}
+int[] array = {24,54,154,8,5,68,69,9,10};
+//array[0]=12;
+
+
+
+int max=Max(
+    Max(array[0],array[1],array[2]),
+    Max(array[3],array[4],array[5]),
+    Max(array[6],array[7],array[8])
+    );
+
+Console.WriteLine(max);
